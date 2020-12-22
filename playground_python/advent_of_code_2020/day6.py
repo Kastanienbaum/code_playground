d3ba93a65591d8a1994ac1fdb3f09b0dd7703f8e
Werner Egermann <egermann@technikum-wien.at>
2020-12-22 19:35:29 +0100

    started day6 pt. 2

---
+++ playground_python/advent_of_code_2020/day6.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3 
+
+# part 1: 
+# What is the sum of those counts? 
+# answer: 6633 
+
+with open('day6_input.txt', 'r') as f: 
+    yes = []
+    total = 0
+    persons = 0
+    # read line, add new chars to list (no duplicates) 
+    for line in f:
+        if line == '\n':
+            total += len(yes)
+            yes = []
+            persons = 0
+        else: 
+            tmp = list(line.split('\n')[0])
+            persons = len(tmp)
+            for x in tmp:
+                if x not in yes:
+                    yes.append(x)
+
+    # take last entry into account
+    total += len(yes)
+    print(total)
+
+
+# EOF 
+
