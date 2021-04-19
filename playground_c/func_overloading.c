bfc3c25f9e5f8648d40ae8324ab6ef996c17736a
Werner Egermann <egermann@technikum-wien.at>
2021-04-19 22:55:44 +0200

    added float function to c operator overloading

--- playground_c/func_overloading.c
+++ playground_c/func_overloading.c
@@ -17,13 +17,20 @@ void fancy_func(int x, int y)
 	printf("%d and %d passed here\n", x, y);
 }
 
+void fancy_func(float x)
+{
+  printf("%.15f passed here\n", x);
+}
+
 
 int main(void)
 {
 	int value = 123; 
+  float fluffy = 3.1415; 
 	fancy_func(value); 
 	fancy_func(); 
 	fancy_func(0x86, value);
+  fancy_func(fluffy);
 
 	return 0;
 }
